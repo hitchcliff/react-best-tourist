1e78102cee4d2271840f167c2fa460c986d44da5
Kevin Nacario <hitchcliff1@gmail.com>
2020-09-04 12:19:44 +0800

    Updates

---
+++ precache-manifest.8bb16cebe6e326a34eaebc310b301587.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "471fe7ef72665d342ee960ce3ed20cf7",
+    "url": "/react-best-tourist/index.html"
+  },
+  {
+    "revision": "1cfa4e6b71d9bb423944",
+    "url": "/react-best-tourist/static/css/main.7574cd87.chunk.css"
+  },
+  {
+    "revision": "92eda25a82ca46cb4bfd",
+    "url": "/react-best-tourist/static/js/2.afcb3401.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-best-tourist/static/js/2.afcb3401.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cfa4e6b71d9bb423944",
+    "url": "/react-best-tourist/static/js/main.9590628b.chunk.js"
+  },
+  {
+    "revision": "99e4769903d20edbcdfd",
+    "url": "/react-best-tourist/static/js/runtime-main.a158fc15.js"
+  },
+  {
+    "revision": "191b834ef6cd2e8395c65f8b78522f3c",
+    "url": "/react-best-tourist/static/media/2.191b834e.jpg"
+  },
+  {
+    "revision": "ddd3b0de9147bed15dcec11321ee86aa",
+    "url": "/react-best-tourist/static/media/4.ddd3b0de.jpeg"
+  },
+  {
+    "revision": "9557f75066d993bbed9b427cb9782e1d",
+    "url": "/react-best-tourist/static/media/5.9557f750.jpg"
+  },
+  {
+    "revision": "2e94b3c4ef6c5eca4292b2f39408ecad",
+    "url": "/react-best-tourist/static/media/6.2e94b3c4.jpg"
+  },
+  {
+    "revision": "342b6622d356521cfa0735453f53bbc8",
+    "url": "/react-best-tourist/static/media/7.342b6622.jpg"
+  },
+  {
+    "revision": "e8d38d3dfdc6e260c365e21cb384e010",
+    "url": "/react-best-tourist/static/media/8.e8d38d3d.jpg"
+  }
+]);
